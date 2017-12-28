8a099bfee2f61219fa69a63ca960a82e62d172f0
Henry Feng <henryzhefeng@gmail.com>
2017-12-28 13:55:06 +0800

    half day commit

--- main.py
+++ main.py
@@ -1,10 +1,67 @@
 '''
 Main file of the Client for ZheQuant
 '''
+import json
+import os
+import requests
 from cmd2 import Cmd
 
 class ZheQuantClient(Cmd):
     '''ZheQuant Client implementation class'''
+
+    def __init__(self):
+        '''Initialization '''
+        super().__init__()
+        # config
+        fname = 'config_default.json'
+        if os.path.isfile('config.json'):
+            fname = 'config.json'
+        with open(fname) as the_file:
+            self.config = json.load(the_file)
+        # session
+        self.session = {}
+
+    def do_login(self, line):
+        '''
+        User login
+
+        Usage: login -u <userId> -p <password>
+        '''
+        url = self.config.server_url + '/user/auth'
+        payload = {
+                "userId"   : "",
+                "password" : ""
+                } #TODO: parse the input and fill the payload
+        rsp_raw = requests.post(url, data=payload)
+        rsp = rsp_raw.json()
+        if rsp_raw.status_code == requests.codes['ok'] \
+            and rsp['success'] is True:
+            self.session['userId'] = rsp['userId']
+            self.session['token'] = rsp['token']
+        else:
+            print('[!] Login failed. Please check your user id or password.')
+
+    def do_schedule(self, line):
+        '''
+        Schedule jobs.
+
+        Usage: schedule -n <job name> -dsc <job description> -t <job type> -p "<arguments>"
+
+        Example: schedule -n my_job -dsc just_for_test -t mv_avg -p "-d 20 -n 5"
+        '''
+        url = self.config.server_url + '/quant/jobs'
+        payload = {
+                "userId"      : "",
+                "token"       : "",
+                "job_name"    : "",
+                "description" : "",
+                "cmd"         : ""
+                } #TODO: parse the input and fill the payload
+        rsp_raw = requests.post(url, data=payload)
+        if rsp_raw.status_code == requests.codes['ok']:
+            print('[x] Job scheduled!')
+        else:
+            print('[!] Scheduling failed!')
 
     def do_mv_avg(self, line):
         '''
@@ -24,16 +81,7 @@ class ZheQuantClient(Cmd):
         #TODO
         pass
 
-    def do_schedule(self, line):
-        '''
-        Schedule jobs.
-
-        Usage: TODO
-        '''
-        #TODO
-        pass
-
-    def do_show(self, line):
+    def do_display(self, line):
         '''
         Show information.
 
@@ -43,9 +91,12 @@ class ZheQuantClient(Cmd):
         pass
 
     def preloop(self):
-        print(  'Welcome to ZheQuant python client!\n' +
-                'Please see the documents here for usage:\n' +
-                'https://github.com/feng-zhe/ZheQuant-client-python');
+        '''
+        Print banner
+        '''
+        print('Welcome to ZheQuant python client!\n'
+                + 'Please see the documents here for usage:\n'
+                + 'https://github.com/feng-zhe/ZheQuant-client-python')
 
 if __name__ == '__main__':
     ZheQuantClient().cmdloop()
